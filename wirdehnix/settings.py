f5f1e83f82073cc12b92d2014faf857a6d4c7c1e
sylviazartmann <sylvi_zartmann@rocketmail.com>
2023-12-05 17:03:12 +0100

    settings allowed hosts

--- wirdehnix/settings.py
+++ wirdehnix/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-l2_aa_ny!r^lo=jsqn86y^0c^jkw#0i=p3o+839ttohvsw5sqi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '34.32.23.93',
+    'localhost',
+    '127.0.0.1',
+    'siehstehnix.sylviazartmann.de'
+]
 
 
 # Application definition
