c033360db529805c54945e1aed96be83d9086a2b
sylviazartmann <sylvi_zartmann@rocketmail.com>
2023-12-06 18:23:48 +0100

    static url 2.0

--- wirdehnix/urls.py
+++ wirdehnix/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + staticfiles_urlpatterns()
