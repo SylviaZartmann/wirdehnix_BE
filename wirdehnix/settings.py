36fb4c8befc3eabaf654e31687250c6bdb036df1
sylviazartmann <sylvi_zartmann@rocketmail.com>
2023-12-08 11:47:55 +0100

    structure apps

--- wirdehnix/settings.py
+++ wirdehnix/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'filmography',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- wirdehnix/urls.py
+++ wirdehnix/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('rest_framework.urls'))
+    #path('filmography/', admin.site.urls),
+    #path('authentication/', admin.site.urls),
 ] + staticfiles_urlpatterns()
