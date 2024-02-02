f6e46988c988b03c38228dbccb2de18d9b79a84b
sylviazartmann <sylvi_zartmann@rocketmail.com>
2024-02-02 20:51:47 +0100

    start changing password and send mail WIP

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from authentication.utils import activate_user, change_password
+from authentication.utils import activate_user, change_password, send_pw_reset_mail
 from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('register/confirm/<str:token>/', activate_user, name='activate_user'),
-    path('changePW/', change_password, name='change_password'),
+    path('register/confirm/<str:token>/', activate_user, name='activate_user'), #send_pw_reset_mail
+    path('mailresetPW/', send_pw_reset_mail, name='reset_password'),
+    path('changePW/confirm/<str:token>/', change_password, name='change_password'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- authentication/utils.py
+++ authentication/utils.py
@@ -5,8 +5,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 # wird in  RegisterView aufgerufen
 def send_activationmail_to_user(new_user, token):
@@ -40,6 +43,27 @@ def gibbet_den_user(email, password):
             return user # wir geben den user als solchen zurück, weil passt
     except get_user_model().DoesNotExist:
         return None
+
+@api_view(('GET', )) # wir haben einen Get Request aus dem Frontend
+@permission_classes((AllowAny,))
+def send_pw_reset_mail(request):
+    try:
+        email = request.data.get('email')
+        user = get_user_model().objects.get(email=email)
+    except ObjectDoesNotExist:
+        raise Response('User not found!', status=status.HTTP_404_NOT_FOUND)
+        
+    token = Token.objects.get_or_create(user=user)
+    
+    reset_link = f"https://wirdehnix.sylviazartmann.de/changePW/confirm/{token}"
+    message = f"Hi {user.username},\n\nPlease click the following link to reset your password:\n{reset_link}"
+    send_mail(
+        "Change Password",
+        message,
+        "contact@sylviazartmann.de",
+        [email],
+    )
+    return Response({'message': 'Password reset link sent successfully.'})
     
     
 @api_view(('POST', )) # wir haben einen Request aus dem Frontend
@@ -49,19 +73,11 @@ def change_password(request):
     new_pw = request.data.get('password')
     new_confpw = request.data.get('conf_password')
 
-    
     if new_pw != new_confpw:
         raise ValidationError("Missmatched passwords.")
 
     user = get_user_model().objects.get(email=email)
     user.set_password(new_pw)
-
-    send_mail(
-        "Your password has been successfully changed",
-        "Your new password is now active.",  
-        "contact@sylviazartmann.de",
-        [email],
-        fail_silently=False,
-    )
+    
     user.save()
     return Response({'message': 'Password changed successfully.'}) 
